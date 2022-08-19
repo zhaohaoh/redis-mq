f0f752cdba96c49ac3b9b0993bdd4c31f0e68bbb
hzh <727172424@qq.com>
2022-08-19 18:15:18 +0800

    1.1.35 手动ack的实现,死信队列配置.消费消息的bug

--- redismq-core/src/main/java/com/redismq/container/RedisMQListenerContainer.java
+++ redismq-core/src/main/java/com/redismq/container/RedisMQListenerContainer.java
@@ -58,7 +58,7 @@ public class RedisMQListenerContainer extends AbstractMessageListenerContainer {
 
     private final ThreadPoolExecutor work = new ThreadPoolExecutor(getConcurrency(), getMaxConcurrency(),
             60L, TimeUnit.SECONDS,
-            new SynchronousQueue<>(), new ThreadFactory() {
+            new LinkedBlockingDeque<>(getConcurrency()), new ThreadFactory() {
         private final ThreadGroup group;
         private final AtomicInteger threadNumber = new AtomicInteger(1);
         private static final String NAME_PREFIX = "redis-mq-delay-work-";
